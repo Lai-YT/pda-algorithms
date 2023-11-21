8589b74e4e30747e8bc7dac05c23b67eec4134f2
Lai-YT <381xvmvbib@gmail.com>
2023-11-21 23:41:07 +0800

    Shrink block size in generated test case
    Reduced block sizes as their scale is irrelevant to floorplanning,
    and larger sizes may cause unnecessary overflows.

--- floorplan/test/gen.py
+++ floorplan/test/gen.py
@@ -13,13 +13,13 @@ class Block:
 def gen_blocks(num_of_blocks: int) -> List[Block]:
     """
     The generated blocks has their width and height uniformly distributed in the
-    range of [30, 120] with the stride of 10.
+    range of [3, 12].
     """
-    RANGE_DIV_10: Final[Tuple[int, int]] = (3, 12)
+    RANGE: Final[Tuple[int, int]] = (3, 12)
     blocks: List[Block] = []
     for _ in range(num_of_blocks):
-        width = randint(*RANGE_DIV_10) * 10
-        height = randint(*RANGE_DIV_10) * 10
+        width = randint(*RANGE)
+        height = randint(*RANGE)
         blocks.append(Block(width, height))
     return blocks
 
@@ -28,6 +28,7 @@ def gen_area_constraint() -> Tuple[float, float]:
     """
     The generated lower bound is [0.3, 0.8] and the upper bound is [1.2, 3.3].
     """
+    # We generate them as integers and later divide to limit the precision of fractional parts.
     LOWER_BOUND_X_100: Tuple[int, int] = (30, 80)
     UPPER_BOUND_X_100: Tuple[int, int] = (120, 330)
     lower_bound: float = randint(*LOWER_BOUND_X_100) / 100
@@ -54,11 +55,12 @@ if __name__ == "__main__":
         description="""
 Generates a list of Blocks to the file {num_of_blocks}.txt.
 The format is as the following:
-
     ```
     <lower bound of the aspect ratio> <upper bound of the aspect ratio>
-    (<block name> <block width> <block height>)+
+    <block name> <block width> <block height>
+    <block name> <block width> <block height>+
     ```
+Ensure that the number of blocks is greater than 1 to make the floorplanning meaningful.
 """,
     )
 
