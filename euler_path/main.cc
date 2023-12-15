f1675888b757c9e15f1e586f0cef48b903c72e8c
Lai-YT <381xvmvbib@gmail.com>
2023-12-15 14:13:42 +0800

    Print path finding result to console

--- euler_path/main.cc
+++ euler_path/main.cc
@@ -47,16 +47,44 @@ int main(int argc, char const* argv[]) {
   auto path_finder = PathFinder{circuit};
   auto [path, edges, hpwl] = path_finder.FindPath();
 
-  std::cout << "=== Connect Path ===" << std::endl;
-  for (const auto& [p, n] : path) {
-    std::cout << p->GetName() << "\t" << n->GetName() << std::endl;
-  }
-  std::cout << "=== Corresponding Net ===" << std::endl;
-  for (const auto& [p, n] : edges) {
-    std::cout << p->GetName() << "\t" << n->GetName() << std::endl;
-  }
-  std::cout << "=== HPWL ===" << std::endl;
+  // The first line gives the total HPWL of all nets in the SPICE netlist.
   std::cout << hpwl << std::endl;
+  // The second and third lines shows the Euler path of the PMOS network in
+  // terms of instance names and net names, respectively.
+  auto prev_p_mos = path.front().first;
+  for (const auto& [p, _] : path) {
+    if (p->GetName() != prev_p_mos->GetName() || p->GetName() != "Dummy") {
+      std::cout << p->GetName() << " ";
+    }
+    prev_p_mos = p;
+  }
+  std::cout << std::endl;
+  auto prev_p_net = edges.front().first;
+  for (const auto& [p, _] : edges) {
+    if (p->GetName() != prev_p_net->GetName() || p->GetName() != "Dummy") {
+      std::cout << p->GetName() << " ";
+    }
+    prev_p_net = p;
+  }
+  std::cout << std::endl;
+  // The fourth and fifth lines shows the Euler path of the NMOS network in
+  // terms of instance names and net names, respectively.
+  auto prev_n_mos = path.front().second;
+  for (const auto& [_, n] : path) {
+    if (n->GetName() != prev_n_mos->GetName() || n->GetName() != "Dummy") {
+      std::cout << n->GetName() << " ";
+    }
+    prev_n_mos = n;
+  }
+  std::cout << std::endl;
+  auto prev_n_net = edges.front().second;
+  for (const auto& [_, n] : edges) {
+    if (n->GetName() != prev_n_net->GetName() || n->GetName() != "Dummy") {
+      std::cout << n->GetName() << " ";
+    }
+    prev_n_net = n;
+  }
+  // No end-of-file newline.
 
   return 0;
 }
